eaf0b65250745fcd503d68c8bbe88aa5426b77c6
Poornima-Raju-Paraganve <104900737+Poornima-Raju-Paraganve@users.noreply.github.com>
2022-05-13 09:25:19 +0530

    Matrixtranspose

---
+++ prg2/src/prg2/MatricsTranspose.java
@@ -0,0 +1,26 @@
+package prg2;
+import java.util.Scanner;
+public class MatricsTranspose {
+     public static void main(String[] args) {
+    	 int original[][]= {{1,2},{3,4}};
+    	 System.out.println("Printing Matrix without transpose:");
+    	 for(int i=0;i<2;i++)
+    			{
+    		 for(int j=0;j<2;j++)
+    		 {
+    			 System.out.print(original [i] [j]+"");
+    		 }
+    		 System.out.println();
+    			 
+    	 }
+    	 System.out.println("Printing Matrix After transpose:");
+    	 for(int i=0;i<2;i++)
+			{
+		 for(int j=0;j<2;j++)
+		 {
+			 System.out.print(original [j] [i]+"");
+		 }
+		 System.out.println();
+		 }
+    	 }
+     }
