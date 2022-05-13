60d2c66f79e454282c3a6c28611625f64c3cd7a4
Poornima-Raju-Paraganve <104900737+Poornima-Raju-Paraganve@users.noreply.github.com>
2022-05-13 09:42:35 +0530

    Concatenation

---
+++ prg8/src/prg8/Concatenation.java
@@ -0,0 +1,18 @@
+package prg8;
+import java.util.Scanner;
+
+public class Concatenation {
+
+	public static void main(String[] args) {
+Scanner sc = new Scanner(System.in);
+		System.out.println("Enter First String :");
+		String firstStr = sc.next();
+		System.out.println("Enter Second String :");
+		String secondStr = sc.next();
+		System.out.println("Result after concatenation:");
+		System.out.println(firstStr+ " " + secondStr);
+	}
+
+	}
+
+
