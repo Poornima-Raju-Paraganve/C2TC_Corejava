e8aaeb5b275a5fcb70ed4c77fb8a627adaf5f265
Poornima-Raju-Paraganve <104900737+Poornima-Raju-Paraganve@users.noreply.github.com>
2022-05-13 09:41:05 +0530

    Recursion

---
+++ prg6/src/prg6/Recursion.java
@@ -0,0 +1,14 @@
+package prg6;
+import java.util.Scanner;
+public class Recursion {
+       public static void main(String[] args) {
+		int number = 123456,reverse = 0;
+		while(number !=0)
+		{
+			int remainder=number % 10;
+			reverse=reverse*10+remainder;
+			number=number/10;
+		}
+		System.out.println("The reverse of the given number is:" +reverse);
+		}
+}
